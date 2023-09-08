fb27879109f41f5afe7cc009745327cf24ee5411
zheng <zheng@zhengdeMacBook-Pro.local>
2023-09-08 22:31:43 +0800

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,29 @@ import { useState } from "react";
 function App() {
   const [cards, setCards] = useState(data);
 
+  function generateAvatar(name, size) {
+    const canvas = document.createElement('canvas');
+    canvas.width = size;
+    canvas.height = size;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#f0f0f0';
+    ctx.fillRect(0, 0, size, size);
+    ctx.font = `${size / 2}px Arial`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = '#333';
+    ctx.fillText(name.substr(0, 1), size / 2, size / 2);
+    return canvas.toDataURL();
+  }
+  
+
   return (
     <>
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-4">Michael&apos; Navigation</h1>
+      <p className="text-center text-gray-500">A simple navigation page for Michael&apos;s daily use.</p>
+    </div>
+    <div>
       {cards.map((card) => (
         <div key={card.cate} className="p-4">
           <h2 className=" font-bold text-lg">{card.cate}</h2>
@@ -16,18 +37,25 @@ function App() {
           lg:grid-cols-3
           md:grid-cols-2
           grid-flow-row auto-rows-max gap-4 mt-4">
-            {card.urls.map((url) => (
-              <a key={url.url} href={url.url}>
-                <Card
-                  icon={url.icon}
-                  title={url.title}
-                  desc={url.desc}
-                />
-              </a>
-            ))}
+            {card.urls.map((url) => {
+              let iconUrl = url.icon;
+              if (!url.icon.startsWith("http") && !url.icon.startsWith("https")) {
+                iconUrl = generateAvatar(url.icon, 50)
+              }
+              return (
+                <a key={url.url} href={url.url}>
+                  <Card
+                    icon={iconUrl}
+                    title={url.title}
+                    desc={url.desc}
+                  />
+                </a>
+              )
+            })}
           </div>
         </div>
       ))}
+      </div>
     </>
   );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,34 @@
 const data = [
     {
+        "cate":"Newsletters",
+        "urls":[
+            {
+                "title":"阮一峰的网络日志",
+                "url":"https://github.com/ruanyf/weekly",
+                "icon":"阮一峰的网络日志",
+                "desc":"科技爱好者周刊."
+            },
+            {
+                "title":"Hacker News",
+                "url":"https://news.ycombinator.com/",
+                "icon":"https://news.ycombinator.com/favicon.ico",
+                "desc":"Hacker News is a social news website focusing on computer science and entrepreneurship."
+            },
+            {
+                "title":"StackExchange-ACADEMIA",
+                "url":"https://academia.stackexchange.com/",
+                "icon":"https://cdn.sstatic.net/Sites/academia/Img/favicon.ico?v=ec617d715196",
+                "desc":"Academia Stack Exchange is a question and answer site for academics and those enrolled in higher education."
+            },
+            {
+                "title":"StackExchange-WORKPLACE",
+                "url":"https://workplace.stackexchange.com/",
+                "icon":"https://cdn.sstatic.net/Sites/workplace/Img/favicon.ico?v=ec617d715196",
+                "desc":"The Workplace Stack Exchange is a question and answer site for members of the workforce navigating the professional setting."
+            }
+        ]
+    },
+    {
         "cate":"UI/UX",
         "urls":[
             {
@@ -31,11 +60,17 @@ const data = [
                 "url":"https://www.linears.art/",
                 "icon":"https://uploads-ssl.webflow.com/638a9c0b679b623167070829/6391a227ea7cec756f7c6f5d_favicon-32x32.png",
                 "desc":"A collection of websites inspired by Linear."
+            },
+            {
+                "title":"中国色-传统颜色",
+                "url":"http://zhongguose.com/",
+                "icon":"http://zhongguose.com/img/favicon.ico",
+                "desc":"Free UI design resources, freebies, mockups, and more."
             }
         ]
     },
     {
-        "cate":"UI/UX Design",
+        "cate":"Front End Development",
         "urls":[
             {
                 "title":"Tailwind CSS",
@@ -48,9 +83,58 @@ const data = [
                 "url":"https://ui.shadcn.com/",
                 "icon":"https://ui.shadcn.com/favicon.ico",
                 "desc":"Beautifully designed components that you can copy and paste into your apps."
-            }
+            },
+            {
+                "title":"Animista",
+                "url":"https://animista.net/",
+                "icon":"https://animista.net/favicon.ico",
+                "desc":"Animista - On-Demand CSS Animations Library."
+            },
+            {
+                "title":"Motion One",
+                "url":"https://motion.dev/",
+                "icon":"https://motion.dev/layout/favicon.png",
+                "desc":"The Web Animations API for everyone."
+            },
+            
         ]
     },
+    {
+        "cate":"Tools",
+        "urls":[
+            {
+                "title":"Repobeats",
+                "url":"https://repobeats.axiom.co/",
+                "icon":"https://repobeats.axiom.co/apple-touch-icon.png",
+                "desc":"Beautiful insights for your GitHub repositories."
+            },
+            {
+                "title":"Shields.io",
+                "url":"https://shields.io/",
+                "icon":"https://shields.io/img/favicon.ico",
+                "desc":"Concise, consistent, and legible badges."
+            },
+            {
+                "title":"Carbon",
+                "url":"https://carbon.now.sh/",
+                "icon":"https://carbon.now.sh/favicon.ico",
+                "desc":"Create and share beautiful images of your source code."
+            },
+            {
+                "title":"grape",
+                "url":"https://www.grape.codes/",
+                "icon":"https://www.grape.codes/grapeLogo.png",
+                "desc":"Create interactive coding videos from your browser."
+            },
+            {
+                "title":"帮小忙",
+                "url":"https://tool.browser.qq.com/",
+                "icon":"https://tool.browser.qq.com/favicon.ico",
+                "desc":"帮小忙，腾讯在线工具箱平台."
+            }
+        ]
+    }
+
 ];
 
 export default data;
