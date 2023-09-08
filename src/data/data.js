a3f616059673e3ded2d6741d5cca009437310936
zheng <zheng@zhengdeMac-mini.local>
2023-09-08 17:54:20 +0800

    add data

--- src/data/data.js
+++ src/data/data.js
@@ -4,39 +4,33 @@ const data = [
         "urls":[
             {
                 "title":"Figma",
-                "url":"https://www.figma.com1/",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
+                "url":"https://www.figma.com/",
+                "icon":"https://static.figma.com/app/icon/1/favicon.png",
+                "desc":"Figma is the leading collaborative design tool for building meaningful products."
             },
             {
-                "title":"Figma",
-                "url":"https://www.figma.com1/7",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
+                "title":"Framer",
+                "url":"https://framer.com/",
+                "icon":"https://app.framerstatic.com/favicon-light@2x-HMWCNIXJ.png",
+                "desc":"Framer is like @Figma but for designing and publishing real responsive websites."
             },
             {
-                "title":"Figma",
-                "url":"https://www.figma.com1/6",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
+                "title":"yesicon",
+                "url":"https://yesicon.app/",
+                "icon":"https://yesicon.app/favicon.svg",
+                "desc":"186,856 枚高品质矢量图标来自全球顶尖设计团队"
             },
             {
-                "title":"Figma",
-                "url":"https://www.figma.com15/",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
+                "title":"Atlas Icons",
+                "url":"https://atlasicons.vectopus.com/",
+                "icon":"https://atlasicons.vectopus.com/favicon.ico",
+                "desc":"2,701Free ConsistentIcons."
             },
             {
-                "title":"Figma",
-                "url":"https://www.figma.com14/",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
-            },
-            {
-                "title":"Figma",
-                "url":"https://www.figma.com13/",
-                "icon":"https://www.figma.com/favicon.ico",
-                "desc":"Figma is a vector graphics editor and prototyping."
+                "title":"Linears Art",
+                "url":"https://www.linears.art/",
+                "icon":"https://uploads-ssl.webflow.com/638a9c0b679b623167070829/6391a227ea7cec756f7c6f5d_favicon-32x32.png",
+                "desc":"A collection of websites inspired by Linear."
             }
         ]
     },
@@ -45,13 +39,13 @@ const data = [
         "urls":[
             {
                 "title":"Tailwind CSS",
-                "url":"https://www.figma.com/12",
+                "url":"https://tailwindcss.com/",
                 "icon":"https://tailwindcss.com/favicons/favicon.ico",
                 "desc":"A utility-first CSS framework for rapidly building custom designs."
             },
             {
                 "title":"shadcn/ui",
-                "url":"https://www.figma.com/33",
+                "url":"https://ui.shadcn.com/",
                 "icon":"https://ui.shadcn.com/favicon.ico",
                 "desc":"Beautifully designed components that you can copy and paste into your apps."
             }
