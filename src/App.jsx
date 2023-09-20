74201a3a5e1ad242868f6b5c6f5e7d413b6b9953
greycode <zhengminghui99@gmail.com>
2023-09-20 11:01:50 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,23 @@ function App() {
     ctx.fillText(name.substr(0, 1), size / 2, size / 2);
     return canvas.toDataURL();
   }
-  
+  // before:w-[356px] before:h-[96px] before:absolute before:top-[2px] before:left-[2px] before:bg-black before:rounded-xl before:z-20 
+
 
   return (
     <>
-    <div className="p-4 mt-4 flex justify-center items-center">
-      <div className="w-[400px] h-[100px] relative overflow-hidden rounded-xl z-0
-      before:w-[396px] before:h-[96px] before:absolute before:top-[2px] before:left-[2px] before:bg-black before:rounded-xl
-      after:w-[500px] after:h-[500px] after:absolute after:top-[-200px] after:left-[-50px] before:z-20 
+    <div className=" min-w-[18rem] p-4 mt-4 flex justify-center items-center">
+      <div className="w-[340px] h-[100px] relative rounded-xl z-0 overflow-hidden
+      after:w-[500px] after:h-[500px] after:absolute after:top-[-200px] after:left-[-50px] 
       after:bg-gradient-to-bl after:from-neutral-800 after:via-indigo-500 after:to-pink-700
       after:z-10 after:animate-spin-slow 
       ">
-        <div className="z-30 absolute top-[15px] left-[20px] h-[70px] w-[360px]">
-          <div className=" text-4xl font-bold text-center leading-tight">Michael&apos; Navigation</div>
-          <div className=" text-center text-gray-500 leading-tight">A simple navigation page for Michael&apos;s daily use.</div>
+        <div className="absolute top-[2px] left-[2px] h-[96px] w-[336px]
+          bg-black rounded-xl z-20 
+          flex flex-col justify-center items-center
+        ">
+          <div className=" text-2xl font-bold text-center leading-tight">Michael&apos; Navigation</div>
+          <div className=" text-sm text-center text-gray-500 leading-tight">A simple navigation page for Michael&apos;s daily use.</div>
         </div>
         
       </div>
